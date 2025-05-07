d72b6f41e72823744e01048b9dd8abcbcd4237e3
munsayac13 <munsayac13@yahoo.com>
2025-05-06 22:19:17 -0500

    Updated fastapi

--- fastapi/fastapi-parameters.py
+++ fastapi/fastapi-parameters.py
@@ -44,3 +44,40 @@ async def get_apostle(apostle_name: ApostleName):
     
     return {"model_name": apostle_name, "message": "{apostle_name} is not on the list of apostles"}
 
+
+###########################################
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# http://127.0.0.1:8000/items/?skip=0&limit=5 returns all three items
+# http://127.0.0.1:8000/items/?skip=1&limit=5 returns only two items
+# http://127.0.0.1:8000/items/?skip=2&limit=5 returns only one item
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_itemtwo(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q":q})
+        return {"item_id": item_id, "q": q}
+    if not short:
+        item.update({"description": "Boolean is not short rather long"})
+        return item
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "Boolean is not short rather long"})
+    return item
+
+
+
