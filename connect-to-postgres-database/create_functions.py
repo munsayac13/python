d7083418ba324cf0e216b521255fff5b029a55a4
munsayac13 <munsayac13@yahoo.com>
2025-05-13 00:05:42 -0500

    Updated python functions

---
+++ connect-to-postgres-database/create_functions.py
@@ -0,0 +1,32 @@
+import psycopg2
+from config import load_config
+import connect
+
+def create_functions():
+    GETOSVERBYDISTROSQL="""
+    CREATE OR REPLACE FUNCTION get_operatingsystems_versions_by_distro(distro VARCHAR) RETURNS TABLE(systemid INTEGER, versionname VARCHAR) AS
+    $$
+    BEGIN
+    	RETURN QUERY
+	
+    	SELECT os.systemid, osv.versionname FROM operatingsystems os
+    		INNER JOIN operatingsystemsversions osv ON os.systemid=osv.systemid
+    		WHERE os.systemname = distro;
+    END; $$
+    LANGUAGE plpgsql;
+    """
+
+    try:
+        config = load_config()
+        with connect.connect_to_database(config) as conn:
+            with conn.cursor() as cur:
+                cur.execute(GETOSVERBYDISTROSQL)
+            cur.close()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)   
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    create_functions()
---
+++ connect-to-postgres-database/query_functions.py
@@ -0,0 +1,23 @@
+import psycopg2
+from config import load_config
+import connect
+
+
+def get_operatingsystems_versions_by_distro(distro):
+    try:
+        config = load_config()
+        with connect.connect_to_database(config) as conn:
+            with conn.cursor() as cur:
+                cur.callproc('get_operatingsystems_versions_by_distro', (distro,))
+                row = cur.fetchone()
+                print(row)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+    finally:
+        if conn:
+            conn.close()    
+
+if __name__ == "__main__":
+    get_operatingsystems_versions_by_distro("DEBIAN")
+    get_operatingsystems_versions_by_distro("UBUNTU")
+    get_operatingsystems_versions_by_distro("CENTOS")
